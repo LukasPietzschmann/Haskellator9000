6447b355873bd4ef028db8c91af29dd67370df5d
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-06-14 13:15:51 +0200

    Added simple tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,23 @@
 module Main (main) where
 
+import Test.Tasty
+import Math.SiConverter.Internal.Lexer (scan)
+import Test.Tasty.HUnit ((@?=), testCase)
+import Math.SiConverter.Internal.Parser (parse)
+import Math.SiConverter.Internal.Expr
+    ( Expr(..), Unit(..), Op(..) )
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests :: TestTree
+unitTests = testGroup "Simple expression parsing" [
+    testCase "Simple integer multiplier" $ parse (scan "1") @?= Value 1 Multiplier,
+    testCase "Simple floating multiplier" $ parse (scan "1.5") @?= Value 1.5 Multiplier,
+    testCase "Simple addition" $ parse (scan "1 + 2") @?= BinOp (Value 1 Multiplier) Plus (Value 2 Multiplier),
+    testCase "Precedence (* before +))" $ parse (scan "1 + 2 * 3") @?= BinOp (Value 1 Multiplier) Plus (BinOp (Value 2 Multiplier) Mult (Value 3 Multiplier)),
+    testCase "Changed precedence (parentheses)" $ parse (scan "(1 + 2) * 3") @?= BinOp (BinOp (Value 1 Multiplier) Plus (Value 2 Multiplier)) Mult (Value 3 Multiplier)
+  ]
