5548a88851022bbd873c330b515e3e3ec6cf7a23
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-06-15 22:36:30 +0200

    Added haskeline as a repl
    This addresses #8

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,30 @@
 module Main where
 
-import System.IO (hFlush, stdout)
+import Control.Monad
+import Control.Monad.IO.Class
 import Math.SiConverter
+import System.Console.Haskeline
 
+exitCommands :: [String]
+exitCommands = ["exit", "quit", ":q", ":quit", ":qa"]
+
+repl :: InputT IO ()
+repl = do
+  minput <- getInputLine "> "
+  case minput of
+    Nothing -> return ()
+    Just input -> do
+      unless (elem input exitCommands) $ do
+        let result = calculate input
+        outputStrLn $ show result
+        repl
+
+replSettings :: MonadIO m => Settings m
+replSettings = Settings {
+    complete = noCompletion,
+    historyFile = Nothing,
+    autoAddHistory = True
+  }
 
 main :: IO ()
-main = do
-  putStr "> "
-  hFlush stdout
-  input <- getLine
-  let result = calculate input
-  print result
-  main
-  return ()
+main = runInputT replSettings repl
