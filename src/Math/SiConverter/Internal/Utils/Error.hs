cac8a38fc2f6ff94402ca96d52857ae6aa816e4a
Thomas Maximilian Schöller <thomas.schoeller@uni-ulm.de>
2024-06-21 14:10:41 +0200

    Fix tests

--- src/Math/SiConverter/Internal/Utils/Error.hs
+++ src/Math/SiConverter/Internal/Utils/Error.hs
@@ -1,8 +1,8 @@
 module Math.SiConverter.Internal.Utils.Error (Error (Error), Kind (..)) where
 
-data Error = Error Kind String
+data Error = Error Kind String deriving Eq
 
 instance Show Error where
   show (Error k m) = show k ++ "\n" ++ m
 
-data Kind = ParseError | ScanError | ImplementationError deriving (Show)
+data Kind = ParseError | ScanError | ImplementationError deriving (Show, Eq)
--- test/Main.hs
+++ test/Main.hs
@@ -5,7 +5,7 @@ module Main (main) where
 import Control.Monad (liftM3)
 import Math.SiConverter.Internal.Expr ( Expr(..), Unit(..), Op(..), Value (..) )
 import Math.SiConverter.Internal.Lexer (scan)
-import Math.SiConverter.Internal.Parser (parse, parseGracefully)
+import Math.SiConverter.Internal.Parser (parse)
 import Math.SiConverter.Internal.Utils.Composition ((.:))
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit ((@?=), testCase)
@@ -29,15 +29,15 @@ tests = testGroup "Tests" [unitTests, propertyTests]
 
 unitTests :: TestTree
 unitTests = testGroup "Simple expression parsing" [
-    testCase "Simple integer multiplier" $ parse (scan "1") @?= Val (Value 1 Multiplier),
-    testCase "Simple floating multiplier" $ parse (scan "1.5") @?= Val (Value 1.5 Multiplier),
-    testCase "Simple addition" $ parse (scan "1 + 2") @?= BinOp (Val $ Value 1 Multiplier) Plus (Val $ Value 2 Multiplier),
-    testCase "Precedence (* before +)" $ parse (scan "1 + 2 * 3") @?= BinOp (Val $ Value 1 Multiplier) Plus (BinOp (Val $ Value 2 Multiplier) Mult (Val $ Value 3 Multiplier)),
-    testCase "Precedence (^ before *)" $ parse (scan "2 ^ 3 * 4") @?= BinOp (BinOp (Val $ Value 2 Multiplier) Pow (Val $ Value 3 Multiplier)) Mult (Val $ Value 4 Multiplier),
-    testCase "Changed precedence (parentheses)" $ parse (scan "(1 + 2) * 3") @?= BinOp (BinOp (Val $ Value 1 Multiplier) Plus (Val $ Value 2 Multiplier)) Mult (Val $ Value 3 Multiplier)
+    testCase "Simple integer multiplier" $ (scan "1" >>= parse) @?= Right (Val (Value 1 Multiplier)),
+    testCase "Simple floating multiplier" $ (scan "1.5" >>= parse) @?= Right (Val (Value 1.5 Multiplier)),
+    testCase "Simple addition" $ (scan "1 + 2" >>= parse) @?= Right (BinOp (Val $ Value 1 Multiplier) Plus (Val $ Value 2 Multiplier)),
+    testCase "Precedence (* before +)" $ (scan "1 + 2 * 3" >>= parse) @?= Right (BinOp (Val $ Value 1 Multiplier) Plus (BinOp (Val $ Value 2 Multiplier) Mult (Val $ Value 3 Multiplier))),
+    testCase "Precedence (^ before *)" $ (scan "2 ^ 3 * 4" >>= parse) @?= Right (BinOp (BinOp (Val $ Value 2 Multiplier) Pow (Val $ Value 3 Multiplier)) Mult (Val $ Value 4 Multiplier)),
+    testCase "Changed precedence (parentheses)" $ (scan "(1 + 2) * 3" >>= parse) @?= Right (BinOp (BinOp (Val $ Value 1 Multiplier) Plus (Val $ Value 2 Multiplier)) Mult (Val $ Value 3 Multiplier))
   ]
 
 propertyTests :: TestTree
 propertyTests = testGroup "Propery tests" [
-    testProperty "Parser should not fail on valid input" $ \x -> either (const False) (const True) (parseGracefully $ scan $ show (x :: Expr))
+    testProperty "Parser should not fail on valid input" $ \x -> either (const False) (const True) (scan (show (x :: Expr)) >>= parse)
   ]
