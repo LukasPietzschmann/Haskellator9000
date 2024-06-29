df87be9f3588cdcd59328e11a2bb43eaf9434136
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-06-29 14:55:12 +0200

    Fixed tests

--- test/Main.hs
+++ test/Main.hs
@@ -23,7 +23,7 @@ genNumber = do
     return $ fromInteger n / 1000.0
 
 instance Arbitrary Expr where
-  arbitrary = let randomNumber = flip (Val .: Value) Multiplier <$> genNumber in frequency [
+  arbitrary = let randomNumber = flip (Val .: Value) (Multiplier 1) <$> genNumber in frequency [
       (10, randomNumber),
       (6, liftM3 BinOp arbitrary arbitrary arbitrary),
       (1, UnaryOp Minus <$> randomNumber)
@@ -33,18 +33,18 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests, propertyTests]
+tests = testGroup "Tests" [simpleExprParseTests, propertyTests]
 
-unitTests :: TestTree
-unitTests = testGroup "Simple expression parsing" [
-    testCase "Simple integer multiplier" $ (scan "1" >>= parse) @?= Right (Val (Value 1 Multiplier)),
-    testCase "Simple floating multiplier" $ (scan "1.5" >>= parse) @?= Right (Val (Value 1.5 Multiplier)),
-    testCase "Simple addition" $ (scan "1 + 2" >>= parse) @?= Right (BinOp (Val $ Value 1 Multiplier) Plus (Val $ Value 2 Multiplier)),
-    testCase "Precedence (* before +)" $ (scan "1 + 2 * 3" >>= parse) @?= Right (BinOp (Val $ Value 1 Multiplier) Plus (BinOp (Val $ Value 2 Multiplier) Mult (Val $ Value 3 Multiplier))),
-    testCase "Precedence (^ before *)" $ (scan "2 ^ 3 * 4" >>= parse) @?= Right (BinOp (BinOp (Val $ Value 2 Multiplier) Pow (Val $ Value 3 Multiplier)) Mult (Val $ Value 4 Multiplier)),
-    testCase "Changed precedence (parentheses)" $ (scan "(1 + 2) * 3" >>= parse) @?= Right (BinOp (BinOp (Val $ Value 1 Multiplier) Plus (Val $ Value 2 Multiplier)) Mult (Val $ Value 3 Multiplier)),
-    testCase "Minus and unary minus" $ (scan "1--2" >>= parse) @?= Right (BinOp (Val $ Value 1 Multiplier) Minus (UnaryOp Minus (Val $ Value 2 Multiplier))),
-    testCase "Mult oper can be omitted" $ (scan "2(3+1)" >>= parse) @?= Right (BinOp (Val $ Value 2 Multiplier) Mult (BinOp (Val $ Value 3 Multiplier) Plus (Val $ Value 1 Multiplier)))
+simpleExprParseTests :: TestTree
+simpleExprParseTests = testGroup "Simple expression parsing" [
+    testCase "Simple integer multiplier" $ (scan "1" >>= parse) @?= Right (Val (Value 1 $ Multiplier 1)),
+    testCase "Simple floating multiplier" $ (scan "1.5" >>= parse) @?= Right (Val (Value 1.5 $ Multiplier 1)),
+    testCase "Simple addition" $ (scan "1 + 2" >>= parse) @?= Right (BinOp (Val $ Value 1 $ Multiplier 1) Plus (Val $ Value 2 $ Multiplier 1)),
+    testCase "Precedence (* before +)" $ (scan "1 + 2 * 3" >>= parse) @?= Right (BinOp (Val $ Value 1 $ Multiplier 1) Plus (BinOp (Val $ Value 2 $ Multiplier 1) Mult (Val $ Value 3 $ Multiplier 1))),
+    testCase "Precedence (^ before *)" $ (scan "2 ^ 3 * 4" >>= parse) @?= Right (BinOp (BinOp (Val $ Value 2 $ Multiplier 1) Pow (Val $ Value 3 $ Multiplier 1)) Mult (Val $ Value 4 $ Multiplier 1)),
+    testCase "Changed precedence (parentheses)" $ (scan "(1 + 2) * 3" >>= parse) @?= Right (BinOp (BinOp (Val $ Value 1 $ Multiplier 1) Plus (Val $ Value 2 $ Multiplier 1)) Mult (Val $ Value 3 $ Multiplier 1)),
+    testCase "Minus and unary minus" $ (scan "1--2" >>= parse) @?= Right (BinOp (Val $ Value 1 $ Multiplier 1) Minus (UnaryOp Minus (Val $ Value 2 $ Multiplier 1))),
+    testCase "Mult oper can be omitted" $ (scan "2(3+1)" >>= parse) @?= Right (BinOp (Val $ Value 2 $ Multiplier 1) Mult (BinOp (Val $ Value 3 $ Multiplier 1) Plus (Val $ Value 1 $ Multiplier 1)))
   ]
 
 propertyTests :: TestTree
