032ed23c58b0d07896276dbf45bda75496fd88a4
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-06-29 19:02:52 +0200

    Simplified tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,17 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
 
 module Main (main) where
 
-import Control.Monad (liftM3)
+import Control.Monad (liftM3, (>=>))
 
+import Math.SiConverter.Internal.Evaluator (evaluate)
 import Math.SiConverter.Internal.Expr (Expr (..), Op (..), Unit (..), Value (..))
 import Math.SiConverter.Internal.Lexer (scan)
 import Math.SiConverter.Internal.Parser (parse)
 import Math.SiConverter.Internal.Utils.Composition ((.:))
+import Math.SiConverter.Internal.Utils.Error (Error)
 
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
@@ -29,6 +33,9 @@ instance Arbitrary Expr where
       (1, UnaryOp Minus <$> randomNumber)
     ]
 
+parseString :: String -> Either Error Expr
+parseString = scan >=> parse
+
 main :: IO ()
 main = defaultMain tests
 
@@ -37,14 +44,14 @@ tests = testGroup "Tests" [simpleExprParseTests, propertyTests]
 
 simpleExprParseTests :: TestTree
 simpleExprParseTests = testGroup "Simple expression parsing" [
-    testCase "Simple integer multiplier" $ (scan "1" >>= parse) @?= Right (Val (Value 1 $ Multiplier 1)),
-    testCase "Simple floating multiplier" $ (scan "1.5" >>= parse) @?= Right (Val (Value 1.5 $ Multiplier 1)),
-    testCase "Simple addition" $ (scan "1 + 2" >>= parse) @?= Right (BinOp (Val $ Value 1 $ Multiplier 1) Plus (Val $ Value 2 $ Multiplier 1)),
-    testCase "Precedence (* before +)" $ (scan "1 + 2 * 3" >>= parse) @?= Right (BinOp (Val $ Value 1 $ Multiplier 1) Plus (BinOp (Val $ Value 2 $ Multiplier 1) Mult (Val $ Value 3 $ Multiplier 1))),
-    testCase "Precedence (^ before *)" $ (scan "2 ^ 3 * 4" >>= parse) @?= Right (BinOp (BinOp (Val $ Value 2 $ Multiplier 1) Pow (Val $ Value 3 $ Multiplier 1)) Mult (Val $ Value 4 $ Multiplier 1)),
-    testCase "Changed precedence (parentheses)" $ (scan "(1 + 2) * 3" >>= parse) @?= Right (BinOp (BinOp (Val $ Value 1 $ Multiplier 1) Plus (Val $ Value 2 $ Multiplier 1)) Mult (Val $ Value 3 $ Multiplier 1)),
-    testCase "Minus and unary minus" $ (scan "1--2" >>= parse) @?= Right (BinOp (Val $ Value 1 $ Multiplier 1) Minus (UnaryOp Minus (Val $ Value 2 $ Multiplier 1))),
-    testCase "Mult oper can be omitted" $ (scan "2(3+1)" >>= parse) @?= Right (BinOp (Val $ Value 2 $ Multiplier 1) Mult (BinOp (Val $ Value 3 $ Multiplier 1) Plus (Val $ Value 1 $ Multiplier 1)))
+    testCase "Simple integer multiplier" $ parseString "1" @?= Right (Val (Value 1 $ Multiplier 1)),
+    testCase "Simple floating multiplier" $ parseString "1.5" @?= Right (Val (Value 1.5 $ Multiplier 1)),
+    testCase "Simple addition" $ parseString "1 + 2" @?= Right (BinOp (Val $ Value 1 $ Multiplier 1) Plus (Val $ Value 2 $ Multiplier 1)),
+    testCase "Precedence (* before +)" $ parseString "1 + 2 * 3" @?= Right (BinOp (Val $ Value 1 $ Multiplier 1) Plus (BinOp (Val $ Value 2 $ Multiplier 1) Mult (Val $ Value 3 $ Multiplier 1))),
+    testCase "Precedence (^ before *)" $ parseString "2 ^ 3 * 4" @?= Right (BinOp (BinOp (Val $ Value 2 $ Multiplier 1) Pow (Val $ Value 3 $ Multiplier 1)) Mult (Val $ Value 4 $ Multiplier 1)),
+    testCase "Changed precedence (parentheses)" $ parseString "(1 + 2) * 3" @?= Right (BinOp (BinOp (Val $ Value 1 $ Multiplier 1) Plus (Val $ Value 2 $ Multiplier 1)) Mult (Val $ Value 3 $ Multiplier 1)),
+    testCase "Minus and unary minus" $ parseString "1--2" @?= Right (BinOp (Val $ Value 1 $ Multiplier 1) Minus (UnaryOp Minus (Val $ Value 2 $ Multiplier 1))),
+    testCase "Mult oper can be omitted" $ parseString "2(3+1)" @?= Right (BinOp (Val $ Value 2 $ Multiplier 1) Mult (BinOp (Val $ Value 3 $ Multiplier 1) Plus (Val $ Value 1 $ Multiplier 1)))
   ]
 
 propertyTests :: TestTree
