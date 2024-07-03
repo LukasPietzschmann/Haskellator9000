8a3482d0d2c45fc78274632f2763dfcbccb89738
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-07-03 11:08:41 +0200

    Fixed precedence of (.:)

--- src/Math/SiConverter/Internal/Utils/Composition.hs
+++ src/Math/SiConverter/Internal/Utils/Composition.hs
@@ -3,5 +3,6 @@ module Math.SiConverter.Internal.Utils.Composition ((.:)) where
 -- | Compose two functions. @f .: g@ is similar to @f . g@
 -- except that @g@ will be fed /two/ arguments instead of one
 -- before handing its result to @f@.
+infixr 8 .:
 (.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
 (.:) = (.) . (.)
