c46293c6fee7f2228b1c0386aed6bdbd9cb8740a
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-07-15 12:12:04 +0200

    Added missing import

--- src/Math/SiConverter/Internal/Expr.hs
+++ src/Math/SiConverter/Internal/Expr.hs
@@ -20,6 +20,7 @@ import Control.Applicative ((<|>))
 import Control.Monad.Except (ExceptT, MonadError (throwError), runExceptT)
 import Control.Monad.State (State, evalState, get, modify)
 
+import Data.List (intercalate)
 import Data.Map (Map, insert, (!?))
 
 import Math.SiConverter.Internal.Operators
